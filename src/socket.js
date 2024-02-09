3f11f1d4a42b510345281a4d7eb74d07bb7755c4
sarthakastic <iamsg26@gmail.com>
2024-02-09 19:49:55 +0530

    production config

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://crayon-server.onrender.com"
+    : "http://localhost:8000";
 
-export const socket = io("http://localhost:8000");
+export const socket = io(URL);
